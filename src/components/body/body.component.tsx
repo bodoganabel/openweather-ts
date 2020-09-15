1aac342a7d820b26e619f2e56a9ca17b50d8d797
bodoganabel <49689981+bodoganabel@users.noreply.github.com>
2020-09-15 16:05:47 +0200

    Added header and body components

---
+++ src/components/body/body.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./body.styles.scss";
+
+const Body = () => {
+
+    return (
+        <div className='body-component'>Hello Body!</div>
+    );
+};
+
+export default Body;
+
+
---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.styles.scss";
+
+const Header = () => {
+
+    return (
+        <div className='header-component'>Hello header!</div>
+    );
+};
+
+export default Header;
+
+
