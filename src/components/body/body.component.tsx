344557226cba99f7f4f24b80d25e8b63d13d39f7
bodoganabel <49689981+bodoganabel@users.noreply.github.com>
2020-09-18 13:39:21 +0200

    WIP: setting data

--- src/components/body/body.component.tsx
+++ src/components/body/body.component.tsx
@@ -1,31 +1,36 @@
-import React from "react";
-import { useState } from 'react';
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./body.styles.scss";
 import WeatherContainer from './weather-container/weather-container.component';
 import LoadingIndicator from "./loading-indicator/loading-indicator.component";
 
 
 export interface IWeatherData {
-    main: string,
-    description: string,
-    temp: number,
-    feelsLike: number,
-    temp_min: number,
-    temp_max: number,
-    pressure: number,
-    humidity: number,
-    windSpeed: number,
-    country: string,
-    city: string,
+    main?: string,
+    description?: string,
+    temp?: number,
+    feelsLike?: number,
+    temp_min?: number,
+    temp_max?: number,
+    pressure?: number,
+    humidity?: number,
+    windSpeed?: number,
+    country?: string,
+    city?: string,
 }
 
 
 const Body = () => {
 
-    const [weatherData, setWeatherData] = useState({});
-    const [startedSearch, setStartedSearch] = useState(false);
+    useEffect(() => {
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=Budapest&appid=dc188740a6faebe07902f134609866d2')
+            .then(response => response.json())
+            .then(response => console.log(response)/*/response => setResult({main: response.main})/*/)
+            .then(response => setWeatherData(response))
+            .catch(error => console.log(error));
+    }, []);
 
+    const [weatherData, setWeatherData] = useState<IWeatherData>(Object);
+    const [startedSearch, setStartedSearch] = useState<boolean>(false);
 
     return (
         <div className='body-component'>
